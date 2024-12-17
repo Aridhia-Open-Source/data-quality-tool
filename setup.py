1019eec3a097378d5b53852d8263dc4d7704770f
jananimurthy <janani.murthy@aridhia.com>
2024-12-17 13:06:45 +0000

    Updated package versions

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+# Function to read the requirements.txt file
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
 setup(
     name='dataqualitycheck',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'polars',
-        'pandas',
-        'reportlab',
-    ],
+    install_requires=read_requirements(),
 )
